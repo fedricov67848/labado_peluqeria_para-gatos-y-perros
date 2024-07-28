d164df20f4b24c7760fac707d140c3b8bce478bc
marcos <richar@gamil.com>
2024-07-28 02:40:02 -0500

    envio datos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("titulo", "labado y peluqeria para gatos y perros")</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header>
